fc07c733c0769f95495f448265b441055032c8ef
borislav <b.manolov@gmail.com>
2014-04-01 21:52:07 +0200

    initial import of current mediawiki

---
+++ languages/messages/MessagesKu_arab.php
@@ -0,0 +1,57 @@
+<?php
+/** كوردي (عەرەبی)‏ (كوردي (عەرەبی)‏)
+ *
+ * See MessagesQqq.php for message documentation incl. usage of parameters
+ * To improve a translation please visit http://translatewiki.net
+ *
+ * @ingroup Language
+ * @file
+ *
+ * @author Arastein
+ * @author Asoxor
+ * @author Bonevarluri
+ * @author George Animal
+ * @author Marmzok
+ * @author Muhammed taha
+ */
+
+$fallback = 'ckb';
+
+$rtl = true;
+
+$digitTransformTable = array(
+	'0' => '٠', # &#x0660;
+	'1' => '١', # &#x0661;
+	'2' => '٢', # &#x0662;
+	'3' => '٣', # &#x0663;
+	'4' => '٤', # &#x0664;
+	'5' => '٥', # &#x0665;
+	'6' => '٦', # &#x0666;
+	'7' => '٧', # &#x0667;
+	'8' => '٨', # &#x0668;
+	'9' => '٩', # &#x0669;
+	'.' => '٫', # &#x066b; wrong table ?
+	',' => '٬', # &#x066c;
+);
+
+$messages = array(
+'underline-never' => 'Qet na',
+
+# Dates
+'january' => 'کانوونی دووەم',
+'january-gen' => 'کانوونی دووەم',
+
+# Categories related messages
+'hidden-category-category' => 'هاوپۆلە شاردراوەکان',
+
+'talkpagelinktext' => 'لێدوان',
+
+# All link text and link target definitions of links into project namespace that get used by other message strings, with the exception of user group pages (see grouppage).
+'aboutsite' => 'دەربارەی {{SITENAME}}',
+'mainpage' => 'دەستپێک',
+'mainpage-description' => 'دەستپێک',
+
+'youhavenewmessagesmulti' => 'شما یه گل پیغوم تازه د $1 داریتو',
+'red-link-title' => '$1 (پەڕە بوونی نییە)',
+
+);
