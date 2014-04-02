b2c985b61ad65c640301a0fff486fb7e2656c9d8
borislav <b.manolov@gmail.com>
2014-04-02 10:51:37 +0200

    add sfb extension

---
+++ extensions/sfb/sfb-converter/test/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+
+spl_autoload_register(function($class){
+	if (strpos($class, 'Sfblib') === 0) {
+		$class = str_replace('_', '/', $class);
+		require_once $class .'.php';
+	}
+});
+
+function addIncludePath($path)
+{
+	if ( is_array($path) ) {
+		$path = implode(PATH_SEPARATOR, $path);
+	}
+	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+}
+addIncludePath(dirname(__FILE__) . '/../lib');
+
+require 'TestCase.php';
---
+++ extensions/sfb/sfb.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once $IP . '/extensions/sfb/sfb-converter/lib/Sfblib/SfbConverter.php';
+require_once $IP . '/extensions/sfb/sfb-converter/lib/Sfblib/SfbToHtmlConverter.php';
+require_once $IP . '/extensions/sfb/sfb-converter/lib/Sfblib/XmlElement.php';
+require_once $IP . '/extensions/sfb/sfb-converter/lib/Sfblib/Char.php';
+
+$wgHooks['ParserFirstCallInit'][] = function(Parser $parser) {
+	$parser->setHook('sfb', function($input, array $args, Parser $parser, PPFrame $frame) {
+		$converter = new Sfblib_SfbToHtmlConverter(trim($input, "\n"));
+		return '<div class="sfb">' . $converter->convert()->getContent() . '</div>';
+	});
+	return true;
+};
+
+$wgHooks['PageContentSave'][] = function($wikiPage, $user, $content, $summary, $isMinor, $isWatch, $section, $flags, $status) {
+	if ($content instanceof ProofreadPageContent) {
+		$content->getBody()->mText = preg_replace_callback('|(<sfb>)(.+)(</sfb>)|Ums', function($matches) {
+			return $matches[1] . Sfblib_SfbConverter::addMissingCommandDelimiters($matches[2]) . $matches[3];
+		}, $content->getBody()->mText);
+	}
+};
